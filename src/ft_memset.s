b0e123bbfe5d4e05d717a2d073dcf2d6aae73a40
Borhink <qhonore@student.42.fr>
2018-05-28 11:53:43 +0200

    ft_memset

---
+++ src/ft_memset.s
@@ -0,0 +1,25 @@
+section .text
+global _ft_memset
+
+; void *ft_memset(void *mem, int c, size_t len);
+;
+; rdi -> void *mem
+; rsi -> int c
+; rdx -> size_t len
+;
+; rax -> return val
+
+_ft_memset:
+    cmp     rdx,        0   ;   if (len == 0)
+    je      return          ;       return;
+	mov		r8,			rdi	;	r8 = rdi (void *mem)
+
+set:	
+	mov     rax,    rsi		;	rax = rsi (int c)
+    mov     rcx,    rdx		;	rcx = rdx (size_t len)
+    cld						;	cld: direction flag -> ++, std: direction flag -> --
+    rep		stosb			;	repeat *rdi = c, ++rdi
+
+return:
+	mov		rax,	r8
+    ret                     ;   return(mem);
