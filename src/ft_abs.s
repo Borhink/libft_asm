6e84811b17f135cea0a742d62402494de4c90800
Borhink <qhonore@student.42.fr>
2018-05-31 16:49:55 +0200

    Update ft_abs.s

--- src/ft_abs.s
+++ src/ft_abs.s
@@ -8,7 +8,7 @@ global _ft_abs
 ; rax -> return val
 
 _ft_abs:
-    cmp     rdi,        0       ;   if (nb > 0)
+    cmp     rdi,        0       ;   if (nb >= 0)
     jge     return              ;       return (nb);
     mov     r8,         0       ;
     sub     r8,         rdi     ;   r8 -= rdi = 0 - -X = 0 + X
